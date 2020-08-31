305417f0dabbc090eec066f7f965819e8a8105b4
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-08-31 20:29:40 +0200

    CHANGE: configure babel in webpack

--- js/modules.js
+++ js/modules.js
@@ -6,35 +6,48 @@
  * @param {string} THIRDPARTY
  * @returns {{rules: Array.<*>}}
  */
-module.exports = (ENV, {MODULES, THIRDPARTY}) => {
-    return {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: new RegExp(`(${MODULES}|${THIRDPARTY})`),
-                use: [
-                    {
-                        loader: "babel-loader"
-                    },
-                    {
-                        // Lint using ESLint
-                        loader: 'eslint-loader',
-                        options: {
-                            fix: true
-                        },
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [{
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images'
-                        }
-                    }
-                ]
+module.exports = (ENV, {
+  MODULES,
+  THIRDPARTY
+}) => {
+  return {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: new RegExp(`(${MODULES}|${THIRDPARTY})`),
+        use: [{
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  "targets": {
+                    "node": "current"
+                  }
+                }],
+                '@babel/preset-react',
+              ],
+              plugins: [
+                "@babel/plugin-proposal-export-default-from",
+              ]
             }
+          },
+          {
+            // Lint using ESLint
+            loader: 'eslint-loader',
+            options: {
+              fix: true
+            },
+          }
         ]
-    };
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'images'
+          }
+        }]
+      }
+    ]
+  };
 };
