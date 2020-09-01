a384e24b7ed09f1c733ad709132cf9e1bba00aea
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-09-01 10:24:39 +0200

    ADD: eslint config

---
+++ eslint-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    indent: ['error', 2],
+    linebreak-style: ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error','always'],
+    no-console: ['warn']
+  }
+};
