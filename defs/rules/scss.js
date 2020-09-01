ba769f66ebd272d0124c599d5fcb6406d5e68b95
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-09-01 10:57:16 +0200

    ADD: change browserslist

--- defs/rules/scss.js
+++ defs/rules/scss.js
@@ -1,20 +1,6 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const autoprefixer = require('autoprefixer');
 
-SUPPORTED_BROWSERS = [
-  ">= 0.2%",
-  "last 1 major version",
-  "not dead",
-  "Chrome >= 45",
-  "Firefox >= 38",
-  "Edge >= 12",
-  "Explorer >= 10",
-  "iOS >= 9",
-  "Safari >= 9",
-  "Android >= 4.4",
-  "Opera >= 30"
-];
-
 /**
  * Export the scss compilation rule
  */
@@ -36,7 +22,7 @@ module.exports = (ENV, {
         loader: 'postcss-loader',
         options: {
           plugins: [
-            autoprefixer({browsers:SUPPORTED_BROWSERS}),
+            autoprefixer({dangerousExtend: true}),
             // require('postcss-inline-svg')({
             //   paths: [
             //     path.resolve(__dirname, 'client/src/img/')
