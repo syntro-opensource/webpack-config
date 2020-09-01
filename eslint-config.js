3918280d158e360a545fc1d103c856da669e97cb
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-09-01 11:37:54 +0200

    ADD: workflow

--- eslint-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    indent: ['error', 2],
-    linebreak-style: ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error','always'],
-    no-console: ['warn']
-  }
-};
