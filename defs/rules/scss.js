abaf44aa37f3a838258302e8d430e9922d0eec3b
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-09-01 10:48:49 +0200

    ADD: postcss config

--- defs/rules/scss.js
+++ defs/rules/scss.js
@@ -1,9 +1,27 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
+
+SUPPORTED_BROWSERS = [
+  ">= 0.2%",
+  "last 1 major version",
+  "not dead",
+  "Chrome >= 45",
+  "Firefox >= 38",
+  "Edge >= 12",
+  "Explorer >= 10",
+  "iOS >= 9",
+  "Safari >= 9",
+  "Android >= 4.4",
+  "Opera >= 30"
+];
 
 /**
  * Export the scss compilation rule
  */
-module.exports = (ENV, {MODULES, THIRDPARTY}) => {
+module.exports = (ENV, {
+  MODULES,
+  THIRDPARTY
+}) => {
   return {
     test: /\.(sa|sc|c)ss$/,
     exclude: new RegExp(`(${MODULES}|${THIRDPARTY})`),
@@ -15,7 +33,18 @@ module.exports = (ENV, {MODULES, THIRDPARTY}) => {
         // options: { modules: /_export.scss$/ }
       },
       {
-        loader: 'postcss-loader'
+        loader: 'postcss-loader',
+        options: {
+          plugins: [
+            autoprefixer({browsers:SUPPORTED_BROWSERS}),
+            require('postcss-inline-svg')({
+              paths: [
+                path.resolve(__dirname, 'client/src/img/')
+              ]
+            }),
+            require('cssnano'),
+          ]
+        }
       },
       {
         loader: 'sass-loader',
